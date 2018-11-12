0b0b4041469b119916a6ff8fb6e8ef3a233f6936
manbirsinghjaspal <manb.jaspal@gmail.com>
2018-11-12 17:04:11 -0500

    variable mutation and Type Conercion

---
+++ script.js
@@ -0,0 +1,21 @@
+var firstName = 'John';
+console.log(firstName);
+
+var lastName = 'SMith';
+console.log(lastName);
+
+var age = 28;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job); //outputs undefined.
+
+//Variable mutation
+
+age = 'twenty eight';  
+alert("the age of john is:" + age);
+
+var name = prompt('WHat is your name?');
+alert(name);
