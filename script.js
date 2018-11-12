631e740faa4adae3d5735a66a8aefaefe54ee787
manbirsinghjaspal <manb.jaspal@gmail.com>
2018-11-12 17:23:24 -0500

    challenge

--- script.js
+++ script.js
@@ -1,21 +1,38 @@
-var firstName = 'John';
-console.log(firstName);
+//var firstName = 'John';
+//console.log(firstName);
+//
+//var lastName = 'SMith';
+//console.log(lastName);
+//
+//var age = 28;
+//
+//var fullAge = true;
+//console.log(fullAge);
+//
+//var job;
+//console.log(job); //outputs undefined.
+//
+////Variable mutation
+//
+//age = 'twenty eight';  
+//alert("the age of john is:" + age);
+//
+//var name = prompt('WHat is your name?');
+//alert(name);
+//
+//console.log(typeof name); // output string
+//
 
-var lastName = 'SMith';
-console.log(lastName);
 
-var age = 28;
+var markHeight = prompt("Mark please enter height in meters");
+var markaMass = prompt("Mark please enter mass in kgs");
+var markBMI = markaMass / (markHeight * markHeight);
+console.log(markBMI);
+var johnHeight = prompt("John please enter height in meters");
+var johnMass = prompt("John please enter mass in kgs");
+var johnBMI = johnMass / (johnHeight * johnHeight);
+console.log(johnBMI);
 
-var fullAge = true;
-console.log(fullAge);
+var BMIcomp = markBMI > johnBMI;
 
-var job;
-console.log(job); //outputs undefined.
-
-//Variable mutation
-
-age = 'twenty eight';  
-alert("the age of john is:" + age);
-
-var name = prompt('WHat is your name?');
-alert(name);
+console.log("Is Marks BMI is higher than Johns? " + BMIcomp); 
