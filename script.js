4d105a2823e288c09320d081808351c64500cb89
manbirsinghjaspal <manb.jaspal@gmail.com>
2018-11-13 13:57:05 -0500

    challenge2

--- script.js
+++ script.js
@@ -40,9 +40,43 @@
 
 //if else statements
 
-var firstName = 'John';
-var civilStatus = 'married';
+//var firstName = 'John';
+//var civilStatus = 'married';
+//
+//if(civilStatus == 'married') {
+//    console.log(firstName + ' is married!');
+//}
 
-if(civilStatus == 'married') {
-    console.log(firstName + ' is married!');
+
+var john1 = 110;
+var john2 = 120;
+var john3 = 103;
+
+var mike1 = 116;
+var mike2 = 94;
+var mike3 = 123;
+
+var avgJohn = (john1 + john2 + john3) / 3;
+var avgMike = (mike1 + mike2 + mike3) / 3;
+
+if (avgJohn > avgMike){
+    console.log("John is the winner with a score of " +avgJohn);
+} else if(avgJohn < avgMike) {
+        console.log("Mike is the winner with a score of " +avgMike);
+} else {
+    console.log("DRAW!!")
+
 }
+ 
+
+
+
+
+
+
+
+
+
+
+
+
