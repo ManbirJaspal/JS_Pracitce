c7f8cb937e0f2067b157da7144ff5e074f0f0dac
manbirsinghjaspal <manb.jaspal@gmail.com>
2018-11-13 14:47:54 -0500

    functions and arrays

--- script.js
+++ script.js
@@ -47,27 +47,54 @@
 //    console.log(firstName + ' is married!');
 //}
 
+//CHALLENGE 2
 
-var john1 = 110;
-var john2 = 120;
-var john3 = 103;
+//var john1 = 110;
+//var john2 = 120;
+//var john3 = 103;
+//
+//var mike1 = 116;
+//var mike2 = 94;
+//var mike3 = 123;
+//
+//var avgJohn = (john1 + john2 + john3) / 3;
+//var avgMike = (mike1 + mike2 + mike3) / 3;
+//
+//if (avgJohn > avgMike){
+//    console.log("John is the winner with a score of " +avgJohn);
+//} else if(avgJohn < avgMike) {
+//        console.log("Mike is the winner with a score of " +avgMike);
+//} else {
+//    console.log("DRAW!!")
+//
+//}
 
-var mike1 = 116;
-var mike2 = 94;
-var mike3 = 123;
+// FUNCTIONS
 
-var avgJohn = (john1 + john2 + john3) / 3;
-var avgMike = (mike1 + mike2 + mike3) / 3;
+//ARRAYS
 
-if (avgJohn > avgMike){
-    console.log("John is the winner with a score of " +avgJohn);
-} else if(avgJohn < avgMike) {
-        console.log("Mike is the winner with a score of " +avgMike);
-} else {
-    console.log("DRAW!!")
+var names = ['John', 'Mark', 'Jane'];
+var years = new Array(1990,1969,1949);
 
-}
- 
+console.log(names[0]);
+console.log(names.length);
+
+names[1] = 'men';
+names[names.length] = 'Mary';
+console.log(names);
+
+
+ //Different Data types
+
+var john = ['John', 'Smith', 1990, 'teacher', false];
+
+john.push('blue'); //adds an element at the end of the array.
+john.unshift('Man'); // adds to the beginning of the array.
+john.pop(); // This removes the element from the end.
+john.shift(); // This removes the first element of the array.
+john.indexOf(1990); // This retrieves the position of the entered element.
+
+
 
 
 
