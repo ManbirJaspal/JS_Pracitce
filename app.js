6572cca0eb30e27ff460e1599680d03aa8ca89b2
manbirsinghjaspal <manb.jaspal@gmail.com>
2018-11-20 20:10:01 -0800

    DOM access and manipulation

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var scores,roundScore, activePlayer, dice;
+var scores,roundScore, activePlayer;
 
 scores = [0,0];
 roundScore = 0;
@@ -10,16 +10,45 @@ console.log(dice);
 
 //Using document Object. we use querySelector Method of the document object to manipulate and change out content on the webpage.
 
-document.querySelector('#current-' + activePlayer).textContent = dice; //using .textContent.
+//document.querySelector('#current-' + activePlayer).textContent = dice; //using .textContent.
 
 //document.querySelector('#current-' + activePlayer).innerHTML = '<em> ' + dice + '</em>'; // using .innerHTML. with this you can use html inside the JS code.
                                                                                         //just that the html content needs to be a string.
 
 
 
-var x = document.querySelector('#score-0').textContent;
+//var x = document.querySelector('#score-0').textContent;
 
-console.log(x);
+
 
 document.querySelector('.dice').style.display = 'none'; //here we access the style method and then the css
                                                         //property that i want to change(diplay, setting to none).
+
+
+//document.querySelector('.btn-roll').addEventListener('click', btn); //to use the btn funvtion as function that is called with an event listener, we use it as btn(without the paranthesis), because we dont want to call right here, we want the event listener to call it for us. And hence, the btn function here is a call back function because its not a function called by us but, by another function.
+
+//We can also write our function inside the addEventListner arguments and it will be an anonymous function which is a fumction which doesnt have a name,
+// and we cannot use this function outside of this context.
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+    //1. RANDOM NUMBER
+    
+    var dice = Math.floor(Math.random() * 6) + 1 // we are declaring the variable in this scope
+    
+    //2. Display the result
+    var diceDOM = document.querySelector('.dice') // here we are putting document.querySelector into a variable for ease of reuasablity.
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png'; //accessing the img tag via the src, and changing the value of src using JS.
+    
+    
+    //3. update the round score only if the roll number was not a 1.
+    //here we use getelementbyid instead of querySelector. Apparently it is faster.
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+});
+
+
+
