e5bff61569f8414a9984876bebbc9e48656bd9f7
manbirsinghjaspal <manb.jaspal@gmail.com>
2019-01-19 14:40:56 -0500

    created the modules

--- budgetapp/app.js
+++ budgetapp/app.js
@@ -7,9 +7,24 @@ var budgetController = (function() {
     }
     
     return {
-        publicTest: function(b) {
-            console.log(add(b));
+        publicTest: function(b) { //we do this so that we can use the add function(which is private) from outside the function.
+            return (add(b));
         }
     }
     
 })();
+
+var UIController = (function() {
+    
+})();
+
+var controller = (function(budgetCtrl, UICtrl) {
+    
+    var z = budgetCtrl.publicTest(5);
+    return {
+        anotherPublic: function() {
+            console.log(z);
+        }
+    }
+    
+})(budgetController, UIController);
